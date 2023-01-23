8dabbd11366652871faa92ffa3ba518258caa69c
Francesca-Barcella <pollon.fra@gmail>
2023-01-23 16:27:37 +0100

    chiamata axios + if else

--- src/App.vue
+++ src/App.vue
@@ -36,11 +36,44 @@ export default {
 }
 </script>
 
+
+
+
 <template>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{url('/')}}">Navbar</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
+                aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNav">
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ url('/') }}" aria-current="page">Home
+                            <span class="visually-hidden">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+
     <section class="vue-home">
         <div class="container">
             <h1>Projects</h1>
-            <!-- <div class="row row-cols-1 row-cols-sm-3">
+            <div v-if="!loading" class="row row-cols-1 row-cols-sm-3 g-4">
                 <div class="col" v-for="project in projects.data">
                     <div class="card">
                         <img class="card-image" src="" alt="">
@@ -49,7 +82,8 @@ export default {
                         </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
+            <div v-else>LOADING...</div>
         </div>
     </section>
 </template>
