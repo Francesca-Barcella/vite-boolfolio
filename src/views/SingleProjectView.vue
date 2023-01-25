1765e2c97653b2405ce33b27b3b0f0aa7f3c6a4f
Francesca-Barcella <pollon.fra@gmail>
2023-01-25 15:29:01 +0100

    fixed bug of stamp single-project

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -17,6 +17,9 @@ export default {
         /******************************************** 
          PROBLEMA!!!
          non funzionano i console log e la chiamata
+         SOLUZIONE!!!
+         impara a fare i console.log a ritroso per capire in che punto si rompe il codice
+         dato che il console.log(response) funzionava perchè in console si vedevano i results del singolo project abbiamo fatto console.log('ERROR') console.log('SUCCESS') per beccare che avevo sscritto succes con un s!!!
          ********************************************/
         //console.log(this.$route.params.slug);
         const url = this.base_api_url + '/api/projects/' + this.$route.params.slug
@@ -24,11 +27,13 @@ export default {
         axios
             .get(url)
             .then(response => {
-                if (response.data.succes) {
+                if (response.data.success) {
+                    // console.log('SUCCESS')
                     this.project = response.data.results
                     this.loading = false
                 } else {
                     /* 404 */
+                    // console.log('ERROR')
                 }
                 console.log(response);
             }).catch(error => {
@@ -40,14 +45,14 @@ export default {
 
 <template>
     <AppBanner :title="$route.params.slug"></AppBanner>
-    <div class="container" v-if="project">
-        <!-- 
-        <h2>{{project.title}}</h2>
-        <div class="description">{{project.description}}</div>
+    <div class="container" v-if="!loading">
+
+        <h2>{{ project.title }}</h2>
+        <div class="description">{{ project.description }}</div>
         <img :src="base_api_url + '/storage/' + project.cover_image" alt="project.title">
-        -->
-        <h2>nome progetto</h2>
-        <div class="description">descrizione progetto</div>
+
+        <!-- <h2>nome progetto</h2>
+        <div class="description">descrizione progetto</div> -->
     </div>
     <div v-else>Sorry, no project here</div>
 
